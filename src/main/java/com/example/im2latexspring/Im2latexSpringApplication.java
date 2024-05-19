cfd6062cf959edf89c00a9dcd597e6139d3cc43a
zxfg223 <zxfg223@gmail.com>
2024-05-19 18:14:06 +0900

    feat : first commit

---
+++ src/main/java/com/example/im2latexspring/Im2latexSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.im2latexspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Im2latexSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Im2latexSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/im2latexspring/Im2latexSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.im2latexspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Im2latexSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
