0edade64b37257ef092cb6e7ebe45e2bb5486349
BigRain02 <zxfg223@gmail.com>
2024-05-20 11:11:17 +0900

    feat : frontend - add LatexRender.vue

--- vue-im2latex/src/App.vue
+++ vue-im2latex/src/App.vue
@@ -1,31 +1,40 @@
 <template>
-    <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js App"/>
-        <ImageUploader />
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ImageUploader />
+    <LatexRender>
+      <p>
+        \[
+        \int_0^\infty e^{-x^2} dx = \frac{\sqrt{\pi}}{2}
+        \]
+      </p>
+    </LatexRender>
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
 import ImageUploader from './components/ImageUploader.vue';
+import LatexRender from './components/LatexRender.vue';
 
 export default {
-    name: 'App',
-    components: {
-        HelloWorld,
-        ImageUploader
-    }
+  name: 'App',
+  components: {
+    HelloWorld,
+    ImageUploader,
+    LatexRender
+  }
 }
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
---
+++ vue-im2latex/src/components/LatexRender.vue
@@ -0,0 +1,28 @@
+<template>
+  <div ref="mathContent">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MathRenderer',
+  mounted() {
+    this.renderMath();
+  },
+  updated() {
+    this.renderMath();
+  },
+  methods: {
+    renderMath() {
+      if (window.MathJax) {
+        window.MathJax.typesetPromise([this.$refs.mathContent])
+            .then(() => {
+              console.log('MathJax: Typeset completed.');
+            })
+            .catch((err) => console.log('MathJax: Typeset failed: ', err));
+      }
+    }
+  }
+}
+</script>
